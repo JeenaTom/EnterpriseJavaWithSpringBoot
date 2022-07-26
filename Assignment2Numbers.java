b0925941c2ac98eaa7b1e254e5da2dcf1bd58cdc
JeenaTom <95923923+JeenaTom@users.noreply.github.com>
2022-07-26 15:11:57 +0530

    Add files via upload

---
+++ Assignment2Numbers.java
@@ -0,0 +1,16 @@
+
+public class Assignment2Numbers {
+
+	public static void main(String[] args) {
+		int count = 4;
+		
+		for (int i=1; i<=count; i++) {
+			for (int j=1; j<=count; j++) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
